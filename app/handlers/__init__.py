ee7dc7feed5c11e1b04659c43ea7d71f9c6bef42
filagate03 <alexfarley555@gmail.com>
2025-10-20 15:59:06 +0300

    Update bot initialization for aiogram 3.7

---
+++ app/handlers/__init__.py
@@ -0,0 +1,3 @@
+from . import admin, balance, buy, payments, start
+
+__all__ = ["admin", "balance", "buy", "payments", "start"]
---
+++ app/services/__init__.py
@@ -0,0 +1,6 @@
+from .admin import AdminService
+from .billing import BillingService
+from .stars import StarsService
+from .users import UserService
+
+__all__ = ["AdminService", "BillingService", "StarsService", "UserService"]
